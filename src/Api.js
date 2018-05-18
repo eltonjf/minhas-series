d413971d334b511fb722ae290f4443a5b445452a
Elton <eltonjf@gmail.com>
2018-05-18 01:03:09 -0300

    Add e listagem de series OK

--- src/Api.js
+++ src/Api.js
@@ -1,13 +1,20 @@
 import axios from 'axios'
+import NewSeries from './NewSeries';
 
 const api = axios.create({
     baseURL: 'http://localhost:3001/'
 })
 
 export const loadGenres =  () => api.get('genres')
+export const saveSeries = (newSeries) => api.post('series', newSeries)
+export const loadSeriesByGenre = (genre) => api.get('series?genre='+genre)
 
 const apis = {
-    loadGenres: loadGenres
+    loadGenres: loadGenres,
+    saveSeries: saveSeries,
+    loadSeriesByGenre: loadSeriesByGenre
 }
+
+api.saveState
 
 export default apis
--- src/App.js
+++ src/App.js
@@ -1,86 +1,55 @@
 import React, { Component } from 'react'
-import axios from 'axios'
-import api from './Api'
+import Home from './Home'
+import Series from './Series'
+
+import{
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import NewSeries from './NewSeries'
+
+
+const About = () => <section className="intro-section"><h1>Sobre</h1></section>
 
 class App extends Component {
 
-  constructor(props){
-    super(props)
-
-    this.state = {
-      genres: [],
-      isLoading: false
-    }
-  }
-
-
-  componentDidMount(){
-    this.setState({isLoading: true})
-      api.loadGenres().
-        then((res)=>{
-          this.setState({
-            isLoading: false,
-            genres: res.data
-          })
-        })
-  }
-
-  renderGenreLik(genre){
-    return (
-      <span>&nbsp;<a href=''>{genre}</a>&nbsp;</span>
-    )
-  }
-
   render() {
     return (
-      <div className="App">
-        <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
-    <div className="container">
-      <div className="navbar-header page-scroll">
-        <a className="navbar-brand page-scroll" href="#page-top">
-            <img src="images/logo.png" height="30" />
-        </a>
-      </div>
+      <Router>
+      <div>
+          <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div className="container">
+              <div className="navbar-header page-scroll">
+                <a className="navbar-brand page-scroll" href="#page-top">
+                    <img src="images/logo.png" height="30" />
+                </a>
+              </div>
 
-      <div className="collapse navbar-collapse navbar-ex1-collapse">
-        <ul className="nav navbar-nav">
-          <li>
-            <a href="">Menu item</a>
-          </li>
-        </ul>
-      </div>
+              <div className="collapse navbar-collapse navbar-ex1-collapse">
+                <ul className="nav navbar-nav">
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
 
-    </div>
-  </nav>
+                  <li>
+                    <Link to="/about">Sobre</Link>
+                  </li>
 
+                  <li>
+                    <Link to="/new">Nova Série</Link>
+                  </li>
+                </ul>
+              </div>
 
-  <section id="intro" className="intro-section">
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-12">
-          <h1><img src="images/logo.png" /></h1>
-          <p>Nunca mais esqueça uma série que você assistiu ou que alguém lhe indicou.</p>
-        </div>
-      </div>
-    </div>
-
-      <section>
-        {
-          this.state.isLoading &&
-          <span>Aguarde, carregando...</span>
-        }
-        {
-          !this.state.isLoading &&
-            <div>
-              Ver séries do Gênero
-                {this.state.genres.map(this.renderGenreLik)}
             </div>
-        }
-      </section>
-
-  </section>
-
-      </div>
+          </nav>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route path='/series/:genre' component={Series} />
+          <Route exact path='/new' component={NewSeries} />
+        </div>
+      </Router>
     );
   }
 }
